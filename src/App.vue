4137e886e5a44eeb6639c08340d44dd69f46f27e
TeckWIT <tant1@wit.edu>
2026-01-21 13:45:54 -0500

    more updated

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <header style="display:flex; gap:12px;">
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-    <RouterLink to="/info">Info</RouterLink>
-  </header>
+  <!-- <header style="display:flex; gap:12px;"> -->
+  <!--   <RouterLink to="/">Home</RouterLink> -->
+  <!--   <RouterLink to="/about">About</RouterLink> -->
+  <!--   <RouterLink to="/info">Info</RouterLink> -->
+  <!-- </header> -->
 
   <RouterView />
 </template>
